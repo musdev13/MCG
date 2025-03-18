de1ae1cfd5f34845359d7c3a3a1a3c266e183911
MusDev <sonics0977703996@gmail.com>
2025-03-18 22:29:14 +0200

    bg and grid

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import pygame
 import sys
+import colors
 
 # Инициализация Pygame
 pygame.init()
 
 # Размеры окна
 WIDTH, HEIGHT = 800, 600
+gridColor = colors.purple
 
 # Создание окна
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Marko Corner")
+
+# Загрузка изображения фона
+background = pygame.image.load("img/dreamW/bg.png")
+# background = pygame.transform.scale(background, (WIDTH, HEIGHT))  # Масштабирование под размер окна
 
 # Основной цикл программы
 running = True
@@ -20,6 +26,17 @@ while running:
 
     # Заливка фона
     screen.fill((0, 0, 0))  # Черный цвет
+    
+
+    # Отображение изображения фона
+    screen.blit(background, (0, 0))
+
+    #Здесь будет отрисовка сетки
+    # Отрисовка сетки
+    for x in range(0, WIDTH, 48):
+        pygame.draw.line(screen, gridColor, (x, 0), (x, HEIGHT))
+    for y in range(0, HEIGHT, 48):
+        pygame.draw.line(screen, gridColor, (0, y), (WIDTH, y))
 
     # Обновление экрана
     pygame.display.flip()
